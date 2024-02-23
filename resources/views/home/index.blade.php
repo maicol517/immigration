f3873695cb74b7dc9ac7d4314a994f7fac6f2a3e
maicol517 <maicol.rojas@gmail.com>
2024-02-23 12:58:13 -0600

    index

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -27,6 +27,17 @@ import 'sweetalert2/src/sweetalert2.scss'
           </div>
         </div>
 
+                <!-- Slide 1 -->
+                <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-2.jpg);">
+                  <div class="carousel-container">
+                    <div class="carousel-content animate__animated animate__fadeInUp">
+                      <h2>Welcome to <span>Company</span></h2>
+                      <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+                      <div class="text-center"><a href="" class="btn-get-started">Read More</a></div>
+                    </div>
+                  </div>
+                </div>
+
 
 
       </div>
